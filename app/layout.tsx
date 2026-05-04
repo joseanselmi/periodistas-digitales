511f44436866a0785f13359ca36d640ea43c2945
unknown <joseanselmi27@gmail.com>
2026-05-04 21:32:00 +0200

    Fix Meta Pixel: move to beforeInteractive Script in head

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,8 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono, Plus_Jakarta_Sans } from "next/font/google";
 import "./globals.css";
 import { Tracker } from "@/components/analytics/tracker"
-import { MetaPixel } from "@/components/analytics/meta-pixel";
+import { MetaPixelEvents } from "@/components/analytics/meta-pixel";
+import Script from "next/script";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -26,6 +27,8 @@ export const metadata: Metadata = {
   description: "Transforma tu experiencia periodística en un medio digital propio que genera ingresos todos los días.",
 };
 
+const PIXEL_ID = '1086780383211630'
+
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -36,8 +39,28 @@ export default function RootLayout({
       lang="es"
       className={`${geistSans.variable} ${geistMono.variable} ${newsreader.variable} h-full antialiased dark`}
     >
+      <head>
+        <Script
+          id="meta-pixel-init"
+          strategy="beforeInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              !function(f,b,e,v,n,t,s)
+              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+              n.queue=[];t=b.createElement(e);t.async=!0;
+              t.src=v;s=b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t,s)}(window, document,'script',
+              'https://connect.facebook.net/en_US/fbevents.js');
+              fbq('init', '${PIXEL_ID}');
+              fbq('track', 'PageView');
+            `,
+          }}
+        />
+      </head>
       <body className="min-h-full flex flex-col bg-black text-white" style={{ fontFamily: 'var(--font-newsreader), sans-serif' }}>
-        <MetaPixel />
+        <MetaPixelEvents />
         <Tracker />
         {children}
       </body>
--- components/analytics/meta-pixel.tsx
+++ components/analytics/meta-pixel.tsx
@@ -1,20 +1,15 @@
 'use client'
 
-import Script from 'next/script'
 import { useEffect } from 'react'
-
-const PIXEL_ID = '1086780383211630'
 
 declare global {
   interface Window {
     fbq: (...args: unknown[]) => void
-    _fbq: unknown
   }
 }
 
-export function MetaPixel() {
+export function MetaPixelEvents() {
   useEffect(() => {
-    // InitiateCheckout on Hotmart link clicks
     const onClick = (e: MouseEvent) => {
       const el = (e.target as HTMLElement).closest('a[href*="hotmart"]') as HTMLAnchorElement | null
       if (el && window.fbq) {
@@ -25,24 +20,5 @@ export function MetaPixel() {
     return () => document.removeEventListener('click', onClick)
   }, [])
 
-  return (
-    <Script
-      id="meta-pixel"
-      strategy="afterInteractive"
-      dangerouslySetInnerHTML={{
-        __html: `
-          !function(f,b,e,v,n,t,s)
-          {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-          n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-          if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-          n.queue=[];t=b.createElement(e);t.async=!0;
-          t.src=v;s=b.getElementsByTagName(e)[0];
-          s.parentNode.insertBefore(t,s)}(window, document,'script',
-          'https://connect.facebook.net/en_US/fbevents.js');
-          fbq('init', '${PIXEL_ID}');
-          fbq('track', 'PageView');
-        `,
-      }}
-    />
-  )
+  return null
 }
